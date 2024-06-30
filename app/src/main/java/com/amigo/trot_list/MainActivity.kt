20e77310cb55762cabb1aa1f6b83ae90f6993fe1
coffeecat432 <coffeecat432@gmail.com>
2024-06-30 18:06:12 +0900

    Initial commit

---
+++ app/src/main/java/com/amigo/trot_list/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.amigo.trot_list
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //1. navigation 만들기
+
+        //2. Fragment만들기 & navigation과 연결하기
+
+        //3. 리사이클러뷰 만들기
+
+
+    }
+}
